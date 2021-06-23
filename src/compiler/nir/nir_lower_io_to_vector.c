79bacbc8a8ed6bd08d2347edcd6b2afc00a75c93
Robert Tarasov <tutankhamen@google.com>
2021-06-23 15:21:23 -0700

    Revert "FROMLIST: nir/lower_io_to_vector: fix per vertex io handling for arrays"
    This reverts commit f53ef010c96b7287eeb396902ef615c2b81ff128.
    
    BUG=b:177028255
    TEST=CtsDeqp on volteer
    
    Change-Id: I47f8dd2bb220e2ffaf523b85f3b71b9488e31b75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/mesa/+/2986079
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Commit-Queue: Ilja H. Friedel <ihf@chromium.org>
    Tested-by: Ilja H. Friedel <ihf@chromium.org>

--- src/compiler/nir/nir_lower_io_to_vector.c
+++ src/compiler/nir/nir_lower_io_to_vector.c
@@ -329,7 +329,7 @@ build_array_deref_of_new_var(nir_builder *b, nir_variable *new_var,
 
 static nir_ssa_def *
 build_array_index(nir_builder *b, nir_deref_instr *deref, nir_ssa_def *base,
-                  bool vs_in, bool per_vertex)
+                  bool vs_in)
 {
    switch (deref->deref_type) {
    case nir_deref_type_var:
@@ -337,13 +337,8 @@ build_array_index(nir_builder *b, nir_deref_instr *deref, nir_ssa_def *base,
    case nir_deref_type_array: {
       nir_ssa_def *index = nir_i2i(b, deref->arr.index.ssa,
                                    deref->dest.ssa.bit_size);
-
-      if (nir_deref_instr_parent(deref)->deref_type == nir_deref_type_var &&
-          per_vertex)
-         return base;
-
       return nir_iadd(
-         b, build_array_index(b, nir_deref_instr_parent(deref), base, vs_in, per_vertex),
+         b, build_array_index(b, nir_deref_instr_parent(deref), base, vs_in),
          nir_amul_imm(b, index, glsl_count_attribute_slots(deref->type, vs_in)));
    }
    default:
@@ -358,15 +353,10 @@ build_array_deref_of_new_var_flat(nir_shader *shader,
 {
    nir_deref_instr *deref = nir_build_deref_var(b, new_var);
 
-   bool per_vertex = nir_is_per_vertex_io(new_var, shader->info.stage);
-   if (per_vertex) {
-      nir_deref_path path;
-      nir_deref_path_init(&path, leader, NULL);
-
-      assert(path.path[0]->deref_type == nir_deref_type_var);
-      nir_deref_instr *p = path.path[1];
-
-      nir_ssa_def *index = p->arr.index.ssa;
+   if (nir_is_per_vertex_io(new_var, shader->info.stage)) {
+      assert(leader->deref_type == nir_deref_type_array);
+      nir_ssa_def *index = leader->arr.index.ssa;
+      leader = nir_deref_instr_parent(leader);
       deref = nir_build_deref_array(b, deref, index);
    }
 
@@ -375,8 +365,8 @@ build_array_deref_of_new_var_flat(nir_shader *shader,
 
    bool vs_in = shader->info.stage == MESA_SHADER_VERTEX &&
                 new_var->data.mode == nir_var_shader_in;
-   return nir_build_deref_array(b, deref,
-      build_array_index(b, leader, nir_imm_int(b, base), vs_in, per_vertex));
+   return nir_build_deref_array(
+      b, deref, build_array_index(b, leader, nir_imm_int(b, base), vs_in));
 }
 
 static bool
