34f766af584203d48bd78cecfbb5da94236f76da
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2023-04-04 20:45:36 +0300

    UPSTREAM: anv: drop lowered storage images code
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Ivan Briano <ivan.briano@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22302>
    (cherry picked from commit 3beaaa9ae8c8a913fb87b4ff10eb8dae8ddda1e8
     https://gitlab.freedesktop.org/mesa/mesa.git main)
    
    Tested dEQP-VK.*storage* on brya with vulkan-cts-1.3.3.1.
      Pass: 46792, Skip: 32672, Duration: 23:09, Remaining: 0
    
    BUG=b:278931070, b:282851667
    TEST=dEQP-VK.*storage*
    
    Change-Id: I7e223244bdef9aad1ddc3fb81e5429895de91c7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/mesa/+/4544420
    Reviewed-by: Sean Paul <sean@poorly.run>
    Commit-Queue: Matt Turner <msturner@google.com>
    Tested-by: Matt Turner <msturner@google.com>
    Reviewed-by: Ilja Friedel <ihf@chromium.org>

--- src/intel/vulkan/anv_private.h
+++ src/intel/vulkan/anv_private.h
@@ -1621,7 +1621,8 @@ struct anv_storage_image_descriptor {
     * SURFACE_STATE table index is in the top 20 bits.
     */
    uint32_t vanilla;
-   uint32_t lowered;
+
+   uint32_t pad;
 };
 
 /** Struct representing a address/range descriptor
@@ -1828,7 +1829,6 @@ struct anv_buffer_view {
 
    struct anv_state surface_state;
    struct anv_state storage_surface_state;
-   struct anv_state lowered_storage_surface_state;
 };
 
 struct anv_push_descriptor_set {
@@ -1985,9 +1985,6 @@ struct anv_pipeline_binding {
       /** Dynamic offset index (for dynamic UBOs and SSBOs) */
       uint8_t dynamic_offset_index;
    };
-
-   /** For a storage image, whether it requires a lowered surface */
-   uint8_t lowered_storage_surface;
 };
 
 struct anv_push_range {
@@ -3988,22 +3985,14 @@ struct anv_image_view {
       struct anv_surface_state general_sampler_surface_state;
 
       /**
-       * RENDER_SURFACE_STATE when using image as a storage image. Separate
-       * states for vanilla (with the original format) and one which has been
-       * lowered to a format suitable for reading.  This may be a raw surface
-       * in extreme cases or simply a surface with a different format where we
-       * expect some conversion to be done in the shader.
+       * RENDER_SURFACE_STATE when using image as a storage image.
        */
       struct anv_surface_state storage_surface_state;
-      struct anv_surface_state lowered_storage_surface_state;
-
-      bool lowered_surface_state_is_null;
    } planes[3];
 };
 
 enum anv_image_view_state_flags {
-   ANV_IMAGE_VIEW_STATE_STORAGE_LOWERED      = (1 << 0),
-   ANV_IMAGE_VIEW_STATE_TEXTURE_OPTIMAL      = (1 << 1),
+   ANV_IMAGE_VIEW_STATE_TEXTURE_OPTIMAL      = (1 << 0),
 };
 
 void anv_image_fill_surface_state(struct anv_device *device,
