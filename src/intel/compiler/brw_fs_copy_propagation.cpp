dae874250819ceb1a3feee0eac9ccf0e98ef31e5
Ian Romanick <ian.d.romanick@intel.com>
2023-06-22 19:03:25 -0700

    UPSTREAM: intel/fs: Constant fold OR and AND
    The path taken in fs_visitor::swizzle_nir_scratch_addr for DG2 generates
    some AND and OR instructions before the SHL. This commit folds those so
    the whold calculation becomes a constant (like on older platforms).
    
    v2: Fix return type of src_as_uint. Noticed by Marcin.
    
    shader-db results:
    
    DG2
    total instructions in shared programs: 23190475 -> 23179540 (-0.05%)
    instructions in affected programs: 36026 -> 25091 (-30.35%)
    helped: 7 / HURT: 0
    
    total cycles in shared programs: 841196807 -> 841142563 (<.01%)
    cycles in affected programs: 1660670 -> 1606426 (-3.27%)
    helped: 7 / HURT: 0
    
    No shader-db changes on any older Intel platforms.
    
    fossil-db results:
    
    DG2
    Totals:
    Instrs: 197780372 -> 197773966 (-0.00%)
    Cycles: 14066410782 -> 14066399378 (-0.00%); split: -0.00%, +0.00%
    Subgroup size: 8438104 -> 8438112 (+0.00%)
    Send messages: 8049445 -> 8049446 (+0.00%)
    Scratch Memory Size: 14263296 -> 14264320 (+0.01%)
    
    Totals from 9 (0.00% of 668055) affected shaders:
    Instrs: 24547 -> 18141 (-26.10%)
    Cycles: 1984791 -> 1973387 (-0.57%); split: -0.98%, +0.40%
    Subgroup size: 88 -> 96 (+9.09%)
    Send messages: 867 -> 868 (+0.12%)
    Scratch Memory Size: 69632 -> 70656 (+1.47%)
    
    No fossil-db changes on any older Intel platforms.
    
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23884>
    (cherry picked from commit cb0de0a1d3420ddf9da56b24b5dd09205b8574ea)
    
    BUG=b:298123643
    TEST=Run dEQP-VK.graphicsfuzz#spv-stable-maze-flatten-copy-composite
    and spv-stable-pillars-volatile-nontemporal-store on Rex.
    
    Change-Id: If2faa28a87d6068805ccd65f85808ba63c8b7294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/mesa/+/4860950
    Reviewed-by: Matt Turner <msturner@google.com>
    Tested-by: Matt Turner <msturner@google.com>
    Commit-Queue: Matt Turner <msturner@google.com>
    Reviewed-by: Sean Paul <sean@poorly.run>

--- src/intel/compiler/brw_fs_copy_propagation.cpp
+++ src/intel/compiler/brw_fs_copy_propagation.cpp
@@ -935,8 +935,6 @@ fs_visitor::try_constant_propagate(fs_inst *inst, acp_entry *entry)
       case BRW_OPCODE_MUL:
       case SHADER_OPCODE_MULH:
       case BRW_OPCODE_ADD:
-      case BRW_OPCODE_OR:
-      case BRW_OPCODE_AND:
       case BRW_OPCODE_XOR:
       case BRW_OPCODE_ADDC:
          if (i == 1) {
@@ -1048,6 +1046,8 @@ fs_visitor::try_constant_propagate(fs_inst *inst, acp_entry *entry)
          }
          break;
 
+      case BRW_OPCODE_AND:
+      case BRW_OPCODE_OR:
       case SHADER_OPCODE_TEX_LOGICAL:
       case SHADER_OPCODE_TXD_LOGICAL:
       case SHADER_OPCODE_TXF_LOGICAL:
@@ -1096,6 +1096,17 @@ fs_visitor::try_constant_propagate(fs_inst *inst, acp_entry *entry)
       default:
          break;
       }
+   }
+
+   /* If only one of the sources of a 2-source, commutative instruction (e.g.,
+    * AND) is immediate, it must be src1. If both are immediate, opt_algebraic
+    * should fold it away.
+    */
+   if (progress && inst->sources == 2 && inst->is_commutative() &&
+       inst->src[0].file == IMM && inst->src[1].file != IMM) {
+      const auto src1 = inst->src[1];
+      inst->src[1] = inst->src[0];
+      inst->src[0] = src1;
    }
 
    return progress;
