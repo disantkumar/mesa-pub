8ef74ce4a3888a2aa84fcf2751acce821db2616f
Francisco Jerez <currojerez@riseup.net>
2023-10-17 18:06:34 -0700

    BACKPORT: UPSTREAM: intel/xehp+: Define driconf option for selectively disabling TBIMR.
    This may help debugging performance problems in the possible case that
    TBIMR negatively impacts the performance of some application.  It could
    also allow applying application-specific band-aid fixes in the XML file
    until a more general workaround is implemented.
    
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25493>
    (cherry picked from commit 694d64188bae3634ea1162773679035874285392
     https://gitlab.freedesktop.org/mesa/mesa.git main)
    
    BUG=b:312204245, b:300500234
    TEST=Run glbench fbofill_tex_bilinear_1024 on Rex
    
    Change-Id: I2c30439a343282c978ec6ed731b4f21a44bbf5bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/mesa/+/5046503
    Commit-Queue: Matt Turner <msturner@google.com>
    Tested-by: Matt Turner <msturner@google.com>
    Reviewed-by: Prahlad Kilambi <prahladk@google.com>
    Auto-Submit: Matt Turner <msturner@google.com>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -89,6 +89,7 @@ static const driOptionDescription anv_dri_options[] = {
       DRI_CONF_ANV_QUERY_COPY_WITH_SHADER_THRESHOLD(6)
       DRI_CONF_ANV_FORCE_INDIRECT_DESCRIPTORS(false)
       DRI_CONF_SHADER_SPILLING_RATE(0)
+      DRI_CONF_OPT_B(intel_tbimr, false, "Enable TBIMR tiled rendering")
    DRI_CONF_SECTION_END
 
    DRI_CONF_SECTION_DEBUG
@@ -1614,6 +1615,7 @@ anv_init_dri_options(struct anv_instance *instance)
        driQueryOptionb(&instance->dri_options, "fake_sparse");
     instance->disable_fcv =
             driQueryOptionb(&instance->dri_options, "anv_disable_fcv");
+    instance->enable_tbimr = driQueryOptionb(&instance->dri_options, "intel_tbimr");
 }
 
 VkResult anv_CreateInstance(
